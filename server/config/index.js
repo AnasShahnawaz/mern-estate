ea98eaf46cb6afadb0aed6a81b84cfa1d3db9261
AnasShahnawaz <118204385+AnasShahnawaz@users.noreply.github.com>
2024-02-03 14:33:28 +0500

    first commit

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 config();
 
-export const port = process.env.PORT;
+export const port = process.env.PORT || 3000;
 export const jwt_secret_key = process.env.JWT_SECRET_KEY;
 export const mongodb_url = process.env.MONGODB_URL;
