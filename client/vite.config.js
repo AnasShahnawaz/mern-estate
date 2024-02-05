77aedbbabec068986469ff9b9b7f3408e8218774
AnasShahnawaz <118204385+AnasShahnawaz@users.noreply.github.com>
2024-02-05 13:36:41 +0500

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-estate-server-phi.vercel.app',
-        secure: true,
-      },
-    },
-  },
   plugins: [react()],
-});
+})
